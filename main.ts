34be361bf72bd5c494822331c4bf54d271ef28f8
mhmoudhadidi <134384059+mhmoudhadidi@users.noreply.github.com>
2023-05-23 13:24:01 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showLeds(`
+         . . # . .
+         . # . # .
+         # # # # #
+         # . . . #
+         # . . . #
+         `)
+})
